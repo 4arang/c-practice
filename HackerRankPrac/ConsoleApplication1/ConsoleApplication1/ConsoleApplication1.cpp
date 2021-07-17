75ac297a35cfbeb50d2d6a232f070e336c998544
4arang <80613153+4arang@users.noreply.github.com>
2021-07-17 17:48:34 +0900

    0717
    0717

--- HackerRankPrac/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ HackerRankPrac/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,78 +5,173 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
-#include <string>
 using namespace std;
-
-
-class Person {
-protected:
-	string name;
-	int age;
-	int cur_id;
-	virtual void getdata() { cin >> this->name >> this->age; }
-	virtual void putdata() {
-		cout << this->name << " " << this->age << endl;
-	}
-};
-
-class Professor : public Person {
-	Professor() { this->cur_id == ++id; }
-
-	int publications;
-	int cur_id;
-	static int id;
-
-	void getdata() // name age publications
-	{
-		cin >> this->name >> this->age >> this->publications;
-	}
-	void putdata() //print name age publications cur_id
-	{
-		cout
-	}
-};
-class Student : public Person {
-
-	int marks[6];
-	void getdata() //name age marks
-	{
-
-	}
-	void putdata()
-	{
-		
-	}
-};
+long long f_x(int n);
+long long g_x(long long n);
 
 int main() {
+	
+	long long q, n;
+	long long m;
+	long long result=0;
 
-	int n, val;
-	cin >> n; //The number of objects that is going to be created.
-	Person *per[n];
-
-	for (int i = 0; i < n; i++) {
-
-		cin >> val;
-		if (val == 1) {
-			// If val is 1 current object is of type Professor
-			per[i] = new Professor;
-
-		}
-		else per[i] = new Student; // Else the current object is of type Student
-
-		per[i]->getdata(); // Get the data from the user.
-
-	}
-
-	for (int i = 0; i < n; i++)
+	cin >> q;
+	while (q--)
 	{
-		per[i]->putdata(); // Print the required output for each object.
-		delete per[i];
+		cin >> n >> m;
+		for(int i = 1; i<=n; i++)
+			result += g_x(i);
+		
+		cout << result%m << endl;
 	}
-	return 0;
 
+	return 0;
 }
+
+long long f_x(int n)
+{
+	long long res=0;
+	long long rres = 0;
+	long long divN;
+	while (n)
+	{
+		divN = n % 10;
+		while (divN)
+		{
+			if (divN != 1)
+			{
+				divN *= (divN - 1);
+				
+			}
+			res += divN;
+			divN--;
+		} 
+		
+		n /= 10;
+	}
+	//////fn///////
+	while (res)
+	{
+		divN = res % 10;
+		rres += divN;
+		res /= 10;
+	}
+	/////sfn/////
+	return rres;
+}
+long long g_x(long long n)
+{
+	int i=0;
+	long long res=0;
+	while (1)
+	{
+		if (f_x(i) == n)
+		{
+			break;
+		}
+		else if (i >= 100000)
+		{
+			cout << "찾을수없음" << endl;
+			return 0;
+		}
+		else
+			i++;
+	}
+	////gi
+
+	long long divN;
+	while (i)
+	{
+		divN = i % 10;
+		res += divN;
+		i /= 10;
+	}
+	//sgi
+
+	return res;
+}
+
+
+
+
+
+//#include <cmath>
+//#include <cstdio>
+//#include <vector>
+//#include <iostream>
+//#include <algorithm>
+//#include <string>
+//using namespace std;
+//
+//
+//class Person {
+//protected:
+//	string name;
+//	int age;
+//	int cur_id;
+//	virtual void getdata() { cin >> this->name >> this->age; }
+//	virtual void putdata() {
+//		cout << this->name << " " << this->age << endl;
+//	}
+//};
+//
+//class Professor : public Person {
+//	Professor() { this->cur_id == ++id; }
+//
+//	int publications;
+//	int cur_id;
+//	static int id;
+//
+//	void getdata() // name age publications
+//	{
+//		cin >> this->name >> this->age >> this->publications;
+//	}
+//	void putdata() //print name age publications cur_id
+//	{
+//		cout
+//	}
+//};
+//class Student : public Person {
+//
+//	int marks[6];
+//	void getdata() //name age marks
+//	{
+//
+//	}
+//	void putdata()
+//	{
+//		
+//	}
+//};
+//
+//int main() {
+//
+//	int n, val;
+//	cin >> n; //The number of objects that is going to be created.
+//	Person *per[n];
+//
+//	for (int i = 0; i < n; i++) {
+//
+//		cin >> val;
+//		if (val == 1) {
+//			// If val is 1 current object is of type Professor
+//			per[i] = new Professor;
+//
+//		}
+//		else per[i] = new Student; // Else the current object is of type Student
+//
+//		per[i]->getdata(); // Get the data from the user.
+//
+//	}
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		per[i]->putdata(); // Print the required output for each object.
+//		delete per[i];
+//	}
+//	return 0;
+//
+//}
 
 
 //#include <iostream>
